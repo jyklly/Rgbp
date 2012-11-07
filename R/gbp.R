022f0fc70a8e9977346d00d3c8bf1286958c47e0
hyungsuktak <hyungsuk.tak@gmail.com>
2012-11-07 17:09:18 -0500

    integrating

--- R/gbp.R
+++ R/gbp.R
@@ -1,8 +1,16 @@
-gbp<-function(arg1,arg2,arg3,arg4,model="gr",intv.alpha,intercept=T){
+gbp<-function(arg1,arg2,mu=NA,X=NA,model="gr",CI=0.95,intercept=T){
 
-	switch(model, 
-		gr=adm(arg1,arg2,arg3,arg4,intv.alpha,intercept=T), 
-		br=bp(arg1,arg2,arg3,arg4,model="br",intv.alpha,intercept=T), 
-		pr=bp(arg1,arg2,arg3,arg4,model="pr",intv.alpha,intercept=T))
+	res<-switch(model, 
+			gr=gr(arg1,arg2,X,mu,CI), 
+			br=bp(arg1,arg2,prior.mean=mu,x=X,model="br",CI,intercept=intercept), 
+			pr=bp(arg1,arg2,prior.mean=mu,x=X,model="pr",CI,intercept=intercept) )
 
+	class(res)<-"gbp"	
+	res
 }
+
+g<-gbp(y,se,mu=8.75,model="gr")
+b<-gbp(z,n,mu=0.265,model="br")
+p<-gbp(z,n,mu=0.265,model="pr")
+
+g
--- R/gbp_print.R
+++ R/gbp_print.R
@@ -1,16 +1,29 @@
 print.gbp<-function(object,...){
 
+######## if model is brimm
+
 	if(object$model=="br"){
 		if(is.na(object$prior.mean)){
 			temp<-data.frame(n=object$n, sample.mean=object$sample.mean, prior.mean.hat=object$p0.hat, shrinkage=object$B.hat, se.shrinkage=object$se.B.hat, post.mean=object$p.hat, se.post.mean=object$se.p.hat, intv.low=object$p.hat.low, intv.upp=object$p.hat.upp)
 		}else{
-			temp<-data.frame(n=object$n, sample.mean=object$sample.mean, prior.mean.hat=object$prior.mean, shrinkage=object$B.hat, se.shrinkage=object$se.B.hat, post.mean=object$p.hat, se.post.mean=object$se.p.hat, intv.low=object$p.hat.low, intv.upp=object$p.hat.upp)	}
+			temp<-data.frame(n=object$n, sample.mean=object$sample.mean, prior.mean=object$prior.mean, shrinkage=object$B.hat, se.shrinkage=object$se.B.hat, post.mean=object$p.hat, se.post.mean=object$se.p.hat, intv.low=object$p.hat.low, intv.upp=object$p.hat.upp)	}
+
+######## if model is primm
 
 	}else if (object$model=="pr"){
 		if(is.na(object$prior.mean)){
 			temp<-data.frame(n=object$n, sample.mean=object$sample.mean, prior.mean.hat=object$lambda0.hat, shrinkage=object$B.hat, se.shrinkage=object$se.B.hat, post.mean=object$lambda.hat, se.post.mean=object$se.lambda.hat, intv.low=object$lambda.hat.low, intv.upp=object$lambda.hat.upp)
 		}else{
-			temp<-data.frame(n=object$n, sample.mean=object$sample.mean, prior.mean.hat=object$prior.mean, shrinkage=object$B.hat, se.shrinkage=object$se.B.hat, post.mean=object$lambda.hat, se.post.mean=object$se.lambda.hat, intv.low=object$lambda.hat.low, intv.upp=object$lambda.hat.upp)	
+			temp<-data.frame(n=object$n, sample.mean=object$sample.mean, prior.mean=object$prior.mean, shrinkage=object$B.hat, se.shrinkage=object$se.B.hat, post.mean=object$lambda.hat, se.post.mean=object$se.lambda.hat, intv.low=object$lambda.hat.low, intv.upp=object$lambda.hat.upp)	
+		}
+
+######## if model is grimm (it is autometically grimm)
+
+	}else if(object$model){
+		if(is.na(object$mu)){
+			temp<-data.frame(y=object$y, se=object$se, mu=object$mu, Bhat=object$Bhat, seB=object$seB, LCL=object$LCL, theta=object$theta, UCL=object$UCL, shat=object$shat)	
+		}else{
+			temp<-data.frame(y=object$y, se=object$se, mu=object$mu, Bhat=object$Bhat, seB=object$seB, LCL=object$LCL, theta=object$theta, UCL=object$UCL, shat=object$shat)	
 		}
 	}
 	temp.mean<-colMeans(temp)
