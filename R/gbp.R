4a37cf27715b13b9dfec91edc50f1e258b8f80ba
Joseph Kelly <jyklly@gmail.com>
2012-11-08 12:42:37 -0500

    nothing major

--- R/gbp.R
+++ R/gbp.R
@@ -4,5 +4,4 @@ gbp<-function(arg1,arg2,arg3,arg4,model="gr",intv.alpha,intercept=T){
 		gr=adm(arg1,arg2,arg3,arg4,intv.alpha,intercept=T), 
 		br=bp(arg1,arg2,arg3,arg4,model="br",intv.alpha,intercept=T), 
 		pr=bp(arg1,arg2,arg3,arg4,model="pr",intv.alpha,intercept=T))
-
 }
