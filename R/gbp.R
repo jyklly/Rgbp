2561600ac98e985f196058e43b140fea185b0391
hyungsuktak <hyungsuk.tak@gmail.com>
2012-11-07 20:05:00 -0500

    work well

--- R/gbp.R
+++ R/gbp.R
@@ -8,7 +8,3 @@ gbp<-function(arg1,arg2,mu=NA,X=NA,model="gr",CI=0.95,intercept=T){
 	class(res)<-"gbp"	
 	res
 }
-
-g<-gbp(y,se,mu=8.75,model="gr")
-b<-gbp(z,n,mu=0.265,model="br")
-p<-gbp(z,n,mu=0.265,model="pr")
