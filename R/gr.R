4ec1d562039dc2bd6257e0cecafd3287fe0cc004
Joseph Kelly <jyklly@gmail.com>
2012-11-09 15:11:51 -0500

    muunknown working

--- R/gr.R
+++ R/gr.R
@@ -6,100 +6,118 @@ library(sn)
 
 gr<-function(y,se,X=NA,mu=NA,CI=0.95){
 	
-##define some values that will be used often
-	muknown <- !is.na(mu)
-	type <- 1
-	k <- length(y)
-	V <- se^2
-	if(!is.na(X))
+  ##define some values that will be used often
+  muknown <- !is.na(mu)
+  type <- 1
+  k <- length(y)
+  V <- se^2
+  if(is.na(X))
     X <- as.matrix(rep(1,k))
 	
-##optimize depending on if mu is specified. 
-	if(muknown){
-		r<-0
-		est <- optim(mean(log(V)),function(x){gr.ll.muknown(exp(x),y,V,mu,type)},lower=0,upper=Inf,control=list(fnscale=-1),method="L-BFGS-B",hessian=T)
-		Ahat <- exp(est$par)
-	}else{
-		r <- dim(X)[2]
-		stop("still in development")
-	}
+  ##optimize depending on if mu is specified. 
+  if(muknown){
+    r<-0
+    est <- optim(mean(log(V)),function(x){gr.ll.muknown(exp(x),y,V,mu,type)},lower=0,upper=Inf,control=list(fnscale=-1),method="L-BFGS-B",hessian=T)
+    est.var <- solve(-1*as.matrix(est$hessian))
+    Ahat <- exp(est$par)
+  }else{
+    r <- dim(X)[2]
+    est <- optim(c(mean(log(V)),rep(0,r)), function(x){gr.ll.muunknown(exp(x[1]),y,V,x[2:length(x)],X,type)},lower=0,upper=Inf,control=list(fnscale=-1),method="L-BFGS-B",hessian=T)
+    est.var <- solve(-1*as.matrix(est$hessian))
+    Ahat <- exp(est$par[1])
+    Betahat <- est$par[2:length(est$par)]
+    BetahatSE <- sqrt(diag(est.var)[-1])
+    mu<-X%*%Betahat
+  }
 	
-##calculate estimates
-	ninfo <- -1*as.matrix(est$hessian)[1,1]
-	est.var <- solve(-1*as.matrix(est$hessian))
-	Avar<-est.var[1,1]*Ahat^2
-	Bhat<-V/(V+Ahat)
-	a0<-ninfo/Bhat
-	a1<-ninfo/(1-Bhat)
-	v<-Bhat*(1-Bhat)/(a1+a0+1)
-	seB<-sqrt(v)
-	skewB<-2*(a0-a1)*sqrt(1+a0+a1)/(sqrt(a0*a1)*(2+a0+a1))
-	LCLB <- qbeta((1-CI)/2,a1,a0)
-	UCLB <- qbeta(1 - (1-CI)/2,a1,a0)
+  ##calculate estimates
+  ninfo <- -1*as.matrix(est$hessian)[1,1]
+  Avar<-est.var[1,1]*Ahat^2
+  Bhat<-V/(V+Ahat)
+  a0<-ninfo/Bhat
+  a1<-ninfo/(1-Bhat)
+  v<-Bhat*(1-Bhat)/(a1+a0+1)
+  seB<-sqrt(v)
+  skewB<-2*(a0-a1)*sqrt(1+a0+a1)/(sqrt(a0*a1)*(2+a0+a1))
+  LCLB <- qbeta((1-CI)/2,a1,a0)
+  UCLB <- qbeta(1 - (1-CI)/2,a1,a0)
+  
+  ##if mu is estimated use different formula for posterior sd
+  if(muknown){
+    shat<-sqrt(V*(1-Bhat)+v*(y-mu)^2)
+  }else{
+    DVAhat<-diag(V+Ahat)
+    ##calculate posterior variance
+    E <- eigen(DVAhat)
+    W <- E$values
+    Q <- E$vectors
+    Z <- Q%*%diag(1/sqrt(W))%*%t(Q)
+    Phat<-Z%*%X%*%solve(t(X)%*%solve(DVAhat)%*%X)%*%t(X)%*%Z
+    p<-diag(Phat)
+    shat<-sqrt((1-(1-p)*Bhat)*V+v*(y-mu)^2)
+  }
+  
+  ##calculate some other moments
+  thetahat<-(1-Bhat)*y+Bhat*mu
+  mu3<-skewB*seB^3
+  skew<-((mu-y)^3*mu3-3*V*(mu-y)*v)/shat^3
+  sqrtV<-sqrt(V)
+  hmB <- length(Bhat)/sum(1/Bhat)
+  m4B <- (6*(a0^3 - a0^2*(2*a1-1) + a1^2*(a1+1)-2*a0*a1*(a1+2))/(a0*a1*(a0+a1+2)*(a0+a1+3)) + 3)*v^2
+  m4 <- 3*V^2*beta(a1+2,a0)/beta(a1,a0) + (mu-y)^4*m4B +6*(beta(a1+3,a0)/beta(a1,a0)*V*(y-mu)^2 - 2*beta(a1+2,a0)/beta(a1,a0)*(y-mu)^2*(1-a0/(a1+a0))*V + V*(1-a0/(a0+a1))^3*(y-mu)^2)
+  kurt <- m4/shat^2
 	
-##if mu is estimated use different formula for posterior sd
-	if(muknown){
-		shat<-sqrt(V*(1-Bhat)+v*(y-mu)^2)
-	}else{
-		stop("still in development")
-	}
-	
-##calculate some other moments
-	thetahat<-(1-Bhat)*y+Bhat*mu
-	mu3<-skewB*seB^3
-	skew<-((mu-y)^3*mu3-3*V*(mu-y)*v)/shat^3
-	sqrtV<-sqrt(V)
-	hmB <- length(Bhat)/sum(1/Bhat)
-	m4B <- (6*(a0^3 - a0^2*(2*a1-1) + a1^2*(a1+1)-2*a0*a1*(a1+2))/(a0*a1*(a0+a1+2)*(a0+a1+3)) + 3)*v^2
-	m4 <- 3*V^2*beta(a1+2,a0)/beta(a1,a0) + (mu-y)^4*m4B +6*(beta(a1+3,a0)/beta(a1,a0)*V*(y-mu)^2 - 2*beta(a1+2,a0)/beta(a1,a0)*(y-mu)^2*(1-a0/(a1+a0))*V + V*(1-a0/(a0+a1))^3*(y-mu)^2)
-	kurt <- m4/shat^2
-	
-## calculate CIs using skewed normal
-## TODO: use apply not loop
-	snparam <- matrix(nrow=length(thetahat),ncol=3)
-	skewedmat <- c()
-	for(i in 1:length(thetahat)){    
-		snparam[i,] <- gr.cp.to.dp(c(thetahat[i],shat[i],sign(skew[i])*min(abs(skew[i]),0.94)))
-		row <- c(qsn((1-CI)/2,snparam[i,1],snparam[i,2],snparam[i,3]),thetahat[i],qsn(1-(1-CI)/2,snparam[i,1],snparam[i,2],snparam[i,3]))
-		skewedmat <- rbind(skewedmat,row)
-	}
-	skewedmat <- as.matrix(skewedmat)
-	
-## return output
-## TODO: discuss with Tak and sync output
-	output<- list(y=y,se=se,mu=mu,Bhat=Bhat,seB=seB,LCL=skewedmat[,1],theta=thetahat,UCL=skewedmat[,3],shat=shat,model="gr")
-	return(output)
+  ## calculate CIs using skewed normal
+  ## TODO: use apply not loop
+  snparam <- matrix(nrow=length(thetahat),ncol=3)
+  skewedmat <- c()
+  for(i in 1:length(thetahat)){    
+    snparam[i,] <- gr.cp.to.dp(c(thetahat[i],shat[i],sign(skew[i])*min(abs(skew[i]),0.94)))
+    row <- c(qsn((1-CI)/2,snparam[i,1],snparam[i,2],snparam[i,3]),thetahat[i],qsn(1-(1-CI)/2,snparam[i,1],snparam[i,2],snparam[i,3]))
+    skewedmat <- rbind(skewedmat,row)
+  }
+  skewedmat <- as.matrix(skewedmat)
+  
+  ## return output
+  ## TODO: discuss with Tak and sync output
+  output<- list(y=y,se=se,mu=mu,Bhat=Bhat,seB=seB,LCL=skewedmat[,1],theta=thetahat,UCL=skewedmat[,3],shat=shat,model="gr")
+  return(output)
 }
 
 ##log adjusted posterior for known mu
 gr.ll.muknown<-function(A,y,V,mu,type){
-	lla<-type*log(A)+sum(dnorm(x=y,mean=mu,sd=sqrt(V+A),log=TRUE))
-	return(lla)
+  lla<-type*log(A)+sum(dnorm(x=y,mean=mu,sd=sqrt(V+A),log=TRUE))
+  return(lla)
+}
+
+##log adjusted posterior for known mu
+gr.ll.muunknown<-function(A,y,V,Beta,X,type){
+  lla<-type*log(A)+sum(dnorm(x=y,mean=X%*%Beta,sd=sqrt(V+A),log=TRUE))
+  return(lla)
 }
 
 ##this function is a slightly modified version of cp.to.dp in the sn package
 ##Author: Adelchi Azzalini
 ##Webpage: http://azzalini.stat.unipd.it/SN/
-gr.cp.to.dp <- function (param) 
-{ 
-	b <- sqrt(2/pi)
-	m <- length(param) - 2
-	gamma1 <- param[m + 2]
-	if (abs(gamma1) > 0.995271746431) 
+gr.cp.to.dp <- function (param) { 
+  b <- sqrt(2/pi)
+  m <- length(param) - 2
+  gamma1 <- param[m + 2]
+  if (abs(gamma1) > 0.995271746431) 
     gamma1 <- 0.9952  ##this line was altered from original
-	A <- sign(gamma1) * (abs(2 * gamma1/(4 - pi)))^(1/3)
-	delta <- A/(b * sqrt(1 + A^2))
-	lambda <- delta/sqrt(1 - delta^2)
-	E.Z <- b * delta
-	sd.Z <- sqrt(1 - E.Z^2)
-	location <- param[1:m]
-	location[1] <- param[1] - param[m + 1] * E.Z/sd.Z
-	scale <- param[m + 1]/sd.Z
-	dp <- c(location, scale, lambda)
-	names(dp)[(m + 1):(m + 2)] <- c("scale", "shape")
-	if (m == 1) 
+  A <- sign(gamma1) * (abs(2 * gamma1/(4 - pi)))^(1/3)
+  delta <- A/(b * sqrt(1 + A^2))
+  lambda <- delta/sqrt(1 - delta^2)
+  E.Z <- b * delta
+  sd.Z <- sqrt(1 - E.Z^2)
+  location <- param[1:m]
+  location[1] <- param[1] - param[m + 1] * E.Z/sd.Z
+  scale <- param[m + 1]/sd.Z
+  dp <- c(location, scale, lambda)
+  names(dp)[(m + 1):(m + 2)] <- c("scale", "shape")
+  if (m == 1) 
     names(dp)[1] <- "location"
-	dp
+  dp
 }
 
 
