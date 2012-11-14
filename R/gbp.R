17508115963c6acaa0617fde71933e8b726bdc07
Joseph Kelly <jyklly@gmail.com>
2012-11-14 12:34:55 -0500

    small

--- R/gbp.R
+++ R/gbp.R
@@ -2,10 +2,14 @@ gbp<-function(arg1,arg2,mu,X,model="gr",CI=0.95,intercept=T,eps=0.0001){
 
   if(missing(X))
     x <- NA
+  else
+    x <- X
+  
   if(missing(mu))
     prior.mean <- NA
+  else
+    prior.mean <- mu
   
-  print(x)
   res<-switch(model, 
               gr=gr(arg1,arg2,X,mu,CI), 
               br=bp(arg1,arg2,prior.mean,x,model="br",CI,intercept=intercept,eps=eps), 
