0ceb28f85cd7f9971d4babb2b6855bc62c317018
Joseph Kelly <jyklly@gmail.com>
2012-11-14 12:00:48 -0500

    baseball name change

--- demo/bp_example.R
+++ demo/bp_example.R
@@ -1,4 +1,13 @@
+library(sn)
+source("../R/gr.R")
+source("../R/gbp.R")
 source("../R/bp.R")
 load("../data/baseball.RData")
+load("../data/schools.RData")
+
+
 br <- bp(baseball$Hits, baseball$At.Bats, model="br")
 pr1 <- bp(baseball$Hits, baseball$At.Bats, model="pr1")
+
+source("../R/gbp_shrinkage_graph.R")
+m1 <- gr(schools$y,schools$se,mu=8)
