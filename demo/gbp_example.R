e856bf7264f6f46ba506c4f14426f3f0ac613e49
Joseph Kelly <jyklly@gmail.com>
2012-11-14 15:13:04 -0500

    bug fix gbp

--- demo/gbp_example.R
+++ demo/gbp_example.R
@@ -7,13 +7,15 @@ load("../data/schools.RData")
 
 
 br <- gbp(baseball$Hits, baseball$At.Bats, model="br")
-pr1 <- gbp(baseball$Hits, baseball$At.Bats, model="pr1")
+pr1 <- gbp(baseball$Hits, baseball$At.Bats, model="pr")
 m1 <- gbp(schools$y,schools$se)
 
 #covariates
-p <- 1
-X <- matrix(rnorm(length(baseball$hits*p)),nrow=length(baseball$hits),ncol=p)
+p <- 2
+X <- matrix(rnorm(length(baseball$Hits)*p),nrow=length(baseball$Hits),ncol=p)
+X
 br <- gbp(baseball$Hits, baseball$At.Bats,X=X, model="br")
-pr1 <- gbp(baseball$Hits, baseball$At.Bats,X=X, model="pr1")
-X <- matrix(rnorm(length(schools$y*p)),nrow=length(schools$y),ncol=p)
-m1 <- gbp(schools$y,schools$se,X=X)
+pr1 <- gbp(baseball$Hits, baseball$At.Bats,X=X, model="pr")
+X <- matrix(rnorm(length(schools$y)*p),nrow=length(schools$y),ncol=p)
+X
+m1 <- gbp(schools$y,schools$se,X=X,model="gr")
