c6ba669ff09c2a0897ba3853af923a00ce2658e3
Joseph Kelly <jyklly@gmail.com>
2013-02-12 11:04:52 -0500

    jumping/not converging

--- Rgbp/R/newton.R
+++ Rgbp/R/newton.R
@@ -1,3 +1,4 @@
+library(gbp)
 data(schools)
 attach(schools)
 
@@ -16,35 +17,44 @@ derval = function(alpha){
   Sigmam = solve(t(X)%*%Wm%*%X)
   Betahat <- solve(t(X)%*%Wm%*%X)%*%t(X)%*%Wm%*%y
 
-  l2 = log(A) - 1/2*sum(log(V+A)) + 1/2*log(det(Sigmam)) - 1/2*sum(wv*(y-X%*%Betahat))
+  l2 = log(A) - 1/2*sum(log(V+A)) + 1/2*log(det(Sigmam)) - 1/2*sum(wv*(y-X%*%Betahat)^2)
 
   dlralphaBEVAL = 1 - A/2*sum(wv) + A/2*tr(Sigmam%*%t(X)%*%Wm^2%*%X) + A/2*sum(wv^2*(y-X%*%Betahat)^2)
   dlralphaBEVAL2 = -A/2*sum(wv^2*V) + A/2*tr(Sigmam%*%t(X)%*%Wm^2%*%X) + A/2*sum(wv^2*(y-X%*%Betahat)^2) +
-    A^2/2*tr((Sigmam%*%t(X)%*%Wm^2%*%X)^2) - A^2*tr(Sigmam%*%t(X)%*%Wm^3%*%X) - A^2*sum(wv^3*(y-X%*%Betahat)^2)
+    A^2/2*tr((Sigmam%*%t(X)%*%Wm^2%*%X)%*%(Sigmam%*%t(X)%*%Wm^2%*%X)) - A^2*tr(Sigmam%*%t(X)%*%Wm^3%*%X) - A^2*sum(wv^3*(y-X%*%Betahat)^2)
 
   dbetahatA = Sigmam%*%t(X)%*%Wm^2%*%(X%*%Betahat - y)
   dbetahatA2 = 2*Sigmam%*%t(X)%*%Wm^2%*%X%*%dbetahatA - 2*Sigmam%*%t(X)%*%Wm^3%*%(X%*%Betahat-y)
-
   dl2alpha = dlralphaBEVAL + A*sum(wv*(y-X%*%Betahat)*X%*%dbetahatA)
   dl2alpha2 = dlralphaBEVAL2 + A*sum(wv^2*V*(y-X%*%Betahat)*X%*%dbetahatA) - A^2*sum(wv*(X%*%dbetahatA)^2) +
     A^2*sum(wv*(y-X%*%Betahat)*X%*%dbetahatA2) - A^2*sum(wv^2*(y-X%*%Betahat)*X%*%dbetahatA)
   return(list(alpha=alpha,l2=l2,dl2alpha=dl2alpha,dl2alpha2=dl2alpha2))
 }
 
-NR = function(vals,tol = 0.000001,maxits = 10){
+NR = function(vals,tol = 0.01,maxits = 100, weight = 1){
 
+  newsor = vals$alpha
   change = 1
   its = 0
   while(change > tol & its < maxits){
     new = vals$alpha - vals$l2/vals$dl2alpha
-    print(new)
-    change = abs((new - vals$alpha)/vals$alpha)
-    vals = derval(new)
+    newsor = (1-weight)*newsor + new*weight
+    change = abs((newsor - vals$alpha)/vals$alpha)
+    vals = derval(newsor)
     its = its + 1
+    print(newsor)
   }
-
+  print(new)
   return(list(est = vals$alpha, der2 = vals$dl2alpha2))
 }
 
-test = NR(derval(-2))
+test = NR(derval(1))
 
+alphavec = seq(-5,15,length.out=1000)
+res = lapply(alphavec,derval)
+l2 <- as.numeric(getattr(res,"l2"))
+dl2alpha <- as.numeric(getattr(res,"dl2alpha"))
+par(mfrow = c(2,1))
+plot(l2~alphavec)
+par(new=true)
+plot(dl2alpha ~ alphavec)
