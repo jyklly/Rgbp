4eb3da6eafa6469f6755b3efb91442eb4f440c90
Joseph Kelly <jyklly@gmail.com>
2013-03-01 12:51:07 -0500

    anal deriv

--- Rgbp/R/newton.R
+++ Rgbp/R/newton.R
@@ -2,14 +2,19 @@ library(gbp)
 data(schools)
 attach(schools)
 
+
+
+
 tr <- function(M){
   sum(diag(M))
 }
 
 V = se^2
 X = matrix(rnorm(length(y)*2),ncol=2)
+source("gr.R")
+a <- gr(y,se,X)
 
-derval = function(alpha){
+derval = function(alpha,y,V,X){
   
   A = exp(alpha)
   wv = 1/(V+A)
@@ -21,8 +26,8 @@ derval = function(alpha){
 
   dlralphaBEVAL = 1 - A/2*sum(wv) + A/2*tr(Sigmam%*%t(X)%*%Wm^2%*%X) + A/2*sum(wv^2*(y-X%*%Betahat)^2)
   dlralphaBEVAL2 = -A/2*sum(wv^2*V) + A/2*tr(Sigmam%*%t(X)%*%Wm^2%*%X) + A/2*sum(wv^2*(y-X%*%Betahat)^2) +
-    A^2/2*tr((Sigmam%*%t(X)%*%Wm^2%*%X)%*%(Sigmam%*%t(X)%*%Wm^2%*%X)) - A^2*tr(Sigmam%*%t(X)%*%Wm^3%*%X) - A^2*sum(wv^3*(y-X%*%Betahat)^2)
-
+    A^2/2*tr((Sigmam%*%t(X)%*%Wm^2%*%X)%*%(Sigmam%*%t(X)%*%Wm^2%*%X)) - A^2*tr(Sigmam%*%t(X)%*%Wm^3%*%X) - A^2*sum(wv^3*(y-X%*%Betahat)^2
+)
   dbetahatA = Sigmam%*%t(X)%*%Wm^2%*%(X%*%Betahat - y)
   dbetahatA2 = 2*Sigmam%*%t(X)%*%Wm^2%*%X%*%dbetahatA - 2*Sigmam%*%t(X)%*%Wm^3%*%(X%*%Betahat-y)
   dl2alpha = dlralphaBEVAL + A*sum(wv*(y-X%*%Betahat)*X%*%dbetahatA)
@@ -37,18 +42,19 @@ NR = function(vals,tol = 0.01,maxits = 100, weight = 1){
   change = 1
   its = 0
   while(change > tol & its < maxits){
-    new = vals$alpha - vals$l2/vals$dl2alpha
+    new = vals$alpha - vals$dl2alpha/vals$dl2alpha2
     newsor = (1-weight)*newsor + new*weight
     change = abs((newsor - vals$alpha)/vals$alpha)
-    vals = derval(newsor)
+    vals = derval(newsor,y,V,X)
     its = its + 1
     print(newsor)
+    print(as.matrix(c(vals$dl2alpha,vals$dl2alpha2)))
   }
-  print(new)
   return(list(est = vals$alpha, der2 = vals$dl2alpha2))
 }
 
-test = NR(derval(1))
+test = NR(derval(log(5),y,V,X),maxits=5,weight=0.1,tol=0.0001)
+#golden section
 
 alphavec = seq(-5,15,length.out=1000)
 res = lapply(alphavec,derval)
@@ -56,5 +62,5 @@ l2 <- as.numeric(getattr(res,"l2"))
 dl2alpha <- as.numeric(getattr(res,"dl2alpha"))
 par(mfrow = c(2,1))
 plot(l2~alphavec)
-par(new=true)
 plot(dl2alpha ~ alphavec)
+
