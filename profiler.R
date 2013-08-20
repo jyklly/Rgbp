02e0c8cd1ccb26dc69c47a66150b994d093ac0d3
Joseph Kelly <jyklly@gmail.com>
2013-08-19 21:16:00 -0700

    arhhh

--- profiler.R
+++ profiler.R
@@ -4,3 +4,10 @@ Rprof("gr.out")
 lapply(1:1000,function(x){gbp(schools$y, schools$se)})
 Rprof(NULL)
 summaryRprof("gr.out")
+
+
+Rprof("qsn.out")
+lapply(1:10000,function(x){qsn(0.75, 1, 2, 1,engine = "T.Owen")})
+Rprof(NULL)
+summaryRprof("qsn.out")
+
