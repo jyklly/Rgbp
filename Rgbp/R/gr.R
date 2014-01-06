144a54af827d2869c2808a7d7e1dd67a6c5f7fb6
Joseph Kelly <jyklly@gmail.com>
2014-01-06 13:34:19 -0500

    gradient changes speedup

--- Rgbp/R/gr.R
+++ Rgbp/R/gr.R
@@ -106,7 +106,7 @@ gr.ll.muknown<-function(A,y,V,mu,type){
   return(lla)
 }
 
-##log adjusted posterior for known mu
+##log adjusted posterior for unknown mu
 gr.ll.muunknown<-function(A,y,V,X,type){
   DVA<-diag(V+A)
   DVAi <- diag(1/(V+A))
@@ -118,7 +118,7 @@ gr.ll.muunknown<-function(A,y,V,X,type){
 ##this function is a slightly modified version of cp.to.dp in the sn package
 ##Author: Adelchi Azzalini
 ##Webpage: http://azzalini.stat.unipd.it/SN/
-gr.cp.to.dp <- function (param) { 
+gr.cp.to.dp <- function (param) {
   b <- sqrt(2/pi)
   m <- length(param) - 2
   gamma1 <- param[m + 2]
@@ -145,20 +145,20 @@ derval <- function(alpha,y,V,X){
   A = exp(alpha)
   wv = 1/(V+A)
   Wm = diag(wv)
-  Sigmam = solve(t(X)%*%Wm%*%X)
-  Betahat <- solve(t(X)%*%Wm%*%X)%*%t(X)%*%Wm%*%y
+  tXwm <- t(X)%*%Wm
+  Sigmam = solve(tXwm%*%X)
+  Betahat <- Sigmam%*%tXwm%*%y
+  res <- (y-X%*%Betahat)
+  exp1 <- Sigmam%*%t(X)%*%Wm^2
+  l2 = log(A) - 1/2*sum(log(V+A)) + 1/2*log(det(Sigmam)) - 1/2*sum(wv*res^2)
 
-  l2 = log(A) - 1/2*sum(log(V+A)) + 1/2*log(det(Sigmam)) - 1/2*sum(wv*(y-X%*%Betahat)^2)
+  dlralphaBEVAL = 1 - A/2*sum(wv) + A/2*tr(exp1%*%X) + A/2*sum(wv^2*res^2)
+  ##dlralphaBEVAL2 = -A/2*sum(wv^2*V) + A/2*tr(Sigmam%*%t(X)%*%Wm^2%*%X) + A/2*sum(wv^2*(y-X%*%Betahat)^2) + A^2/2*tr((Sigmam%*%t(X)%*%Wm^2%*%X)%*%(Sigmam%*%t(X)%*%Wm^2%*%X)) - A^2*tr(Sigmam%*%t(X)%*%Wm^3%*%X) - A^2*sum(wv^3*(y-X%*%Betahat)^2)
 
-  dlralphaBEVAL = 1 - A/2*sum(wv) + A/2*tr(Sigmam%*%t(X)%*%Wm^2%*%X) + A/2*sum(wv^2*(y-X%*%Betahat)^2)
-  dlralphaBEVAL2 = -A/2*sum(wv^2*V) + A/2*tr(Sigmam%*%t(X)%*%Wm^2%*%X) + A/2*sum(wv^2*(y-X%*%Betahat)^2) +
-    A^2/2*tr((Sigmam%*%t(X)%*%Wm^2%*%X)%*%(Sigmam%*%t(X)%*%Wm^2%*%X)) - A^2*tr(Sigmam%*%t(X)%*%Wm^3%*%X) - A^2*sum(wv^3*(y-X%*%Betahat)^2)
-
-  dbetahatA = Sigmam%*%t(X)%*%Wm^2%*%(X%*%Betahat - y)
-  dbetahatA2 = 2*Sigmam%*%t(X)%*%Wm^2%*%X%*%dbetahatA - 2*Sigmam%*%t(X)%*%Wm^3%*%(X%*%Betahat-y)
-  dl2alpha = dlralphaBEVAL + A*sum(wv*(y-X%*%Betahat)*X%*%dbetahatA)
-  dl2alpha2 = dlralphaBEVAL2 + A*sum(wv^2*V*(y-X%*%Betahat)*X%*%dbetahatA) - A^2*sum(wv*(X%*%dbetahatA)^2) +
-    A^2*sum(wv*(y-X%*%Betahat)*X%*%dbetahatA2) - A^2*sum(wv^2*(y-X%*%Betahat)*X%*%dbetahatA)
+  dbetahatA = exp1%*%res
+  ## dbetahatA2 = 2*Sigmam%*%t(X)%*%Wm^2%*%X%*%dbetahatA - 2*Sigmam%*%t(X)%*%Wm^3%*%(X%*%Betahat-y)
+  dl2alpha = dlralphaBEVAL + A*sum(wv*res*X%*%dbetahatA)
+  ##dl2alpha2 = dlralphaBEVAL2 + A*sum(wv^2*V*(y-X%*%Betahat)*X%*%dbetahatA) - A^2*sum(wv*(X%*%dbetahatA)^2) + A^2*sum(wv*(y-X%*%Betahat)*X%*%dbetahatA2) - A^2*sum(wv^2*(y-X%*%Betahat)*X%*%dbetahatA)
 
   ## return(list(alpha=alpha,l2=l2,dl2alpha=dl2alpha,dl2alpha2=dl2alpha2))
   return(dl2alpha=dl2alpha)
